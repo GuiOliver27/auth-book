eeefbbf251add1312773381453be20b0f657ff70
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-05-02 01:40:24 -0400

    Feat: Implement the getSignInKey method

--- src/main/java/com/security/jwt/config/JwtService.java
+++ src/main/java/com/security/jwt/config/JwtService.java
@@ -2,11 +2,33 @@ package com.security.jwt.config;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 @Service
 public class JwtService {
+
+    @SuppressWarnings("unused")
+    private static final String SECRET_KEY = "ebhfpTglOvUuWHPcK7BCGVN4imNDkUmG";
 
     public String extractUsername(String token) {
         return null;
     }
 
+    private JwtParserBuilder extractAllClaims(String token) {
+        return Jwts
+            .parser()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
