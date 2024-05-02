b39ef5cb410f802ae0315c55366f2d72e8fdb3c8
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-05-01 20:27:12 -0400

    Feat: Transform the User to an Entity and extend the de user to UserDetails object

--- src/main/java/com/security/jwt/user/User.java
+++ src/main/java/com/security/jwt/user/User.java
@@ -1,6 +1,15 @@
 package com.security.jwt.user;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -14,8 +23,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
-public class User {
+@Table(name = "_user")
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
@@ -23,4 +32,44 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    
 }
