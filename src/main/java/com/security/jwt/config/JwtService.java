a04aa5b996a9d2f30234ed9e4751c462eee36f94
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-05-06 02:54:29 -0400

    Feat: Generate the JWT token

--- src/main/java/com/security/jwt/config/JwtService.java
+++ src/main/java/com/security/jwt/config/JwtService.java
@@ -1,5 +1,11 @@
 package com.security.jwt.config;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.hibernate.mapping.Map;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
@@ -7,6 +13,7 @@ import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureAlgorithm;
 
 @Service
 public class JwtService {
@@ -15,10 +22,29 @@ public class JwtService {
     private static final String SECRET_KEY = "ebhfpTglOvUuWHPcK7BCGVN4imNDkUmG";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
-    private JwtParserBuilder extractAllClaims(String token) {
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(
+    Map<String, Object> extraClaims,
+    UserDetails userDetails
+  ) {
+    return Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+    private Claims extractAllClaims(String token) {
         return Jwts
             .parser()
             .setSigningKey(getSignInKey())
