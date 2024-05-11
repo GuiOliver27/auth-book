fabcdd8bb51d4dff1c9c3336a8fe560d5a36389a
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-05-11 00:50:45 -0400

    Feat: Implement the authenticate method

--- src/main/java/com/security/jwt/auth/AuthenticationService.java
+++ src/main/java/com/security/jwt/auth/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.security.jwt.auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,24 +17,32 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
-            .firstname(request.getFirstname())
-            .lastname(request.getLastname())
-            .email(request.getEmail())
-            .password(passwordEncoder.encode(request.getPassword()))
-            .build();
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
         repository.save(user);
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
-            .token(jwtToken)
-            .build();
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse aunthenticate(RegisterRequest request) {
-
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+            .orElseThrow();
         return null;
     }
-    
+
 }
